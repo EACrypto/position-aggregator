0b9a87c74376f8486095272d01fad9c9597a13c3
Andrew Chen <andrew.chen305@gmail.com>
2018-04-21 20:18:17 -0400

    utc time

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ BASE_URI = "https://www.bitmex.com/api/v1/trade?"
 COLS = "side%2C%20size%2C%20price"
 
 def get_all_trades(instrument, start_time, end_time, page_num = 0):
+    print("Working on page {}...".format(str(page_num)))
     base_query_url = \
         BASE_URI + "symbol={}&columns={}&startTime={}&endTime={}&count=500" \
                         .format(instrument, COLS, start_time, end_time)
@@ -20,10 +21,6 @@ def get_all_trades(instrument, start_time, end_time, page_num = 0):
     else:
         print("Request error: {}".format(response))
         sys.exit()
-
-    if len(jData) == 500:
-        sleep(0.1)
-        jData.extend(get_all_trades(instrument, start_time, end_time, page_num + 1))
     return jData
 
 def agg_tradeflow(trades):
@@ -38,9 +35,24 @@ def agg_tradeflow(trades):
 
 if __name__ == '__main__':
     instrument = "XBT"
-    start_time = "2018-04-15%2020%3A05%3A00"
-    end_time = str(datetime.datetime.now())
-    jData = get_all_trades(instrument, start_time, end_time)
-    buys, sells = agg_tradeflow(jData)
+    time_delta = datetime.timedelta(minutes = 240)
+    end_time = datetime.datetime.utcnow()
+    start_time = end_time - time_delta
+    # start_time = "2018-04-14 13:00:00"
+    # end_time = "2018-04-15 20:00:00"
+
+    data = []
+    new_data = get_all_trades(instrument, str(start_time), str(end_time))
+    data.extend(new_data)
+    page_num = 0
+    while len(new_data) == 500:
+        sleep(1.5)
+        page_num += 1
+        new_data = get_all_trades(instrument, start_time, end_time, page_num)
+        data.extend(new_data)
+
+    buys, sells = agg_tradeflow(data)
+    print("Start Time: {}".format(str(start_time)))
+    print("End Time: {}".format(str(end_time)))
     print("B: {:,}".format(buys))
     print("S: {:,}".format(sells))
